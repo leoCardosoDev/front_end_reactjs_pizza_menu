a9ff7a2477e79b1781436834bff10592c801773f
Leonardo Cardoso de Oliveira <leocardosodev@gmail.com>
2024-08-26 16:23:51 -0700

    feat: add responsive layout

--- src/index.js
+++ src/index.js
@@ -123,14 +123,15 @@ function Order({ closeHour, openHour }) {
 }
 
 function Pizza({ pizzaObj }) {
-  if (pizzaObj.soldOut) return null;
   return (
-    <li className="pizza">
+    <li className={`pizza ${pizzaObj.soldOut ? 'sold-out' : ''}`}>
       <img src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span>R$ {pizzaObj.price},00</span>
+        <span>
+          {pizzaObj.soldOut ? 'Vendido' : 'R$ ' + pizzaObj.price + ',00'}
+        </span>
       </div>
     </li>
   );
